40d911190d7cf0f29dc3c43f9e9c56445afd3611
Soma Limbek <soma.limbek@gmail.com>
2019-11-26 15:37:15 +0100

    Initial Commit

---
+++ SetAndConcentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetAndConcentration
+//
+//  Created by Limbek Soma on 2019. 11. 26..
+//  Copyright © 2019. Soma Limbek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
